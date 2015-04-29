898700550256704b2f4f51e71e8d1741c68c98b2
Maxime <maxime.launay@gmail.com>
2015-04-29 17:15:44 +0200

    Update Structure

---
+++ Powershell/Set-Configuration.ps1
@@ -0,0 +1,103 @@
+﻿<# 
+.SYNOPSIS 
+    Upload configuration files used to create the environment and update parameters.json.
+.DESCRIPTION 
+    Upload configuration files used to create the environment. Configuration Files are :
+        - .\ResourcesManager\environnement.json : contains all resources who should be deployed
+        - .\DSC\environmentDSC.ps1 : contains all DSC configurations who should be push to IaaS VMs
+        - .\SQL\ConfigurationFile.ini : File use to set an unattend installation of SQL Server
+        - .\SQL\database.sql : SQL Script use to create a database in the IaaS SQL Server
+    Update parameters.json to use your own storage account and containers
+    You must have an active Azure Subscription tu ose this script
+.EXAMPLE 
+   Set-Configuration.ps1 ` 
+        -SubscriptionName "YourSubcription" `
+        -StorageAccountName "YourStorageAccount" `
+        -ContainerName "YourContainer" `
+        -Location = "Your Azure Datacenter (eg : North Europe)" `
+        -ParametersFile "..\Sample\ARM\parameters.json" `
+        -TemplateParametersFile "..\Sample\ARM\parameters_template.json" `
+        -DSCFile "..\Sample\DSC\environmentDSC.ps1" `
+        -SQLSetupConfigurationFile "..\Sample\SQL\ConfigurationFile.ini" `
+        -SQLdatabaseFile "..\Sample\SQL\database.sql" `
+        -WebPackageFile "..\Sample\WebPackage\WebPackage.zip"
+.OUTPUTS 
+    no output
+#>
+
+param (
+    [Parameter(Mandatory = $true)][string]$SubscriptionName,
+    [Parameter(Mandatory = $true)][string]$StorageAccountName,
+    [Parameter(Mandatory = $true)][string]$ContainerName,
+    [Parameter(Mandatory = $true)][string]$Location,
+    [Parameter(Mandatory = $true)][string]$ParametersFile,
+    [Parameter(Mandatory = $true)][string]$TemplateParametersFile,
+    [Parameter(Mandatory = $true)][string]$DSCFile,
+    [Parameter(Mandatory = $true)][string]$SQLSetupConfigurationFile,
+    [Parameter(Mandatory = $true)][string]$SQLdatabaseFile,
+    [Parameter(Mandatory = $true)][string]$WebPackageFile
+)
+
+# Switch to Service Management Mode
+Switch-AzureMode -Name AzureServiceManagement
+
+#Select-AzureSubscription $subscriptionName
+# Set Azure Subscription and current Storage Account
+Set-AzureSubscription `
+    -SubscriptionName $subscriptionName `
+    -CurrentStorageAccountName $StorageAccountName
+
+try{
+    # Create a storage account for Configuration Files if it does not exist
+    if (!((Get-AzureStorageAccount).StorageAccountName -contains $StorageAccountName)){
+        New-AzureStorageAccount -StorageAccountName $StorageAccountName `
+            -Location $Location | Out-Null
+        write-verbose "Storage Account $StorageAccountName created"
+    }
+    
+    # create a container (with public blob) for Configuration Files if it does not exist
+    if (!((Get-AzureStorageContainer).Name -contains $ContainerName)){
+        New-AzureStorageContainer -Name $ContainerName -Permission Blob | out-null
+        write-verbose "Container $ContainerName created"
+    }
+
+    # get Parameters.json
+    $ParametersFileObj = Get-Content $TemplateParametersFile -raw | ConvertFrom-Json
+
+    # Upload DSC File File
+    Publish-AzureVMDscConfiguration `
+        -ConfigurationPath $DSCFile `
+        -containerName $ContainerName `
+        -Force
+    write-output "- File '$DSCFile' push to the container '$ContainerName' on the storage account '$StorageAccountName'"
+    
+    # Set DSCModuleURI
+    $ParametersFileObj.DSCModuleURI.value = "https://$StorageAccountName.blob.core.windows.net/$ContainerName/" `
+        + (Get-Item $DSCFile).Name + ".zip"
+    
+    # Upload SQL Setup Configuration File
+    Set-AzureStorageBlobContent -Container $ContainerName -File $SQLSetupConfigurationFile -Force | out-Null
+    write-output "- File '$SQLSetupConfigurationFile' push to the container '$ContainerName' on the storage account '$StorageAccountName'"
+    
+    # Set SQLSetupConfigurationFileUri
+    $ParametersFileObj.SQLSetupConfigurationFileUri.value = "https://$StorageAccountName.blob.core.windows.net/$ContainerName/" `
+        + (Get-Item $SQLSetupConfigurationFile).Name
+    
+    # Upload SQL Database File
+    Set-AzureStorageBlobContent -Container $ContainerName -File $SQLdatabaseFile -Force | out-Null
+    write-output "- File '$SQLdatabaseFile' push to the container '$ContainerName' on the storage account '$StorageAccountName'"
+    
+    # Set SQLDatabaseFileUri
+    $ParametersFileObj.SQLSetupConfigurationFileUri.value = "https://$StorageAccountName.blob.core.windows.net/$ContainerName/" `        + (Get-Item $SQLdatabaseFile).Name
+    
+    # Upload Web Package File
+    Set-AzureStorageBlobContent -Container $ContainerName -File $WebPackageFile -Force | out-Null
+    write-output "- File '$WebPackageFile' push to the container '$ContainerName' on the storage account '$StorageAccountName'"
+
+    # Save Parameters.json
+    $ParametersFileObj | ConvertTo-Json | Out-File $ParametersFile
+}catch{
+    write-host "Error during Files upload"
+    throw $_
+    #exit 1
+}
---
+++ Powershell/Startup.ps1
@@ -0,0 +1,66 @@
+<# 
+.SYNOPSIS 
+    Set configuration to use this project
+.DESCRIPTION
+    Set configuration to use this project and run a basic deployment :
+        - 1 IaaS VM for Active Directory
+        - 1 IaaS VM for SQL Server
+        - 1 Azure Web Site
+    Only SubscriptionName, StorageAccountName and envName must be set (because here must be unique in Azure). All other 
+    parameters are in this script and in .\ResourcesManager\environment_template.json file.
+    You must have an active Azure Subscription and set an Azure Account (Add-AzureAccount) before used this script
+.EXAMPLE 
+    ./Startup.ps1
+.OUTPUTS 
+   no output
+#>
+
+param (
+    [string]$SubscriptionName = "Osiatis CIS - MSDN Dev-Test", # Specify our Azure Subscription Name
+    [string]$StorageAccountName = "sourcedatafiles",
+    [string]$envName = "armasset" # Lower Case, 11 chars max
+)
+
+Switch-AzureMode -Name AzureServiceManagement
+
+# Set variables
+
+[string]$ContainerName = "configurationfiles"
+[string]$DSCFile = "..\Sample\DSC\environmentDSC.ps1"
+[string]$SQLSetupConfigurationFile = "..\Sample\SQL\ConfigurationFile.ini"
+[string]$SQLdatabaseFile = "..\Sample\SQL\database.sql"
+[string]$WebPackageFile = "..\Sample\WebPackage\WebPackage.zip"
+[string]$Location = "North Europe"
+[string]$ParametersFile = "..\Sample\ARM\parameters.json"
+[string]$TemplateFile = "..\Sample\ARM\environnement.json"
+[string]$TemplateParametersFile = "..\Sample\ARM\parameters_template.json"
+
+# Internal Variables
+$DSCArchive = "$DSCFile.zip"
+
+Set-AzureSubscription `
+    -SubscriptionName $subscriptionName `
+    -CurrentStorageAccountName $StorageAccountName
+
+# push Configuration Files to the container of the storage account
+.\Set-Configuration.ps1 `
+    -SubscriptionName $SubscriptionName `
+    -StorageAccountName $StorageAccountName `
+    -ContainerName $ContainerName `
+    -Location $Location `
+    -ParametersFile $ParametersFile `
+    -DSCFile $DSCFile `
+    -SQLSetupConfigurationFile $SQLSetupConfigurationFile `
+    -SQLdatabaseFile $SQLdatabaseFile `
+    -WebPackageFile $WebPackageFile `
+    -TemplateParametersFile $TemplateParametersFile
+
+.\Set-Environment-With-Parameters-File.ps1 `
+    -TemplateFile $TemplateFile `
+    -ParametersFile $ParametersFile `
+    -envName $envName `
+    -subscriptionName $subscriptionName `
+    -Location $Location `
+    -StorageAccountName $StorageAccountName `
+    -ContainerName $ContainerName `
+    -DSCArchive $DSCArchive
